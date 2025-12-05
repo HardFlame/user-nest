778d0559e3785bc568cd0714c26f5eb00616c1bf
HardFlame <gmhardflame@gmail.com>
2025-12-06 00:16:14 +0400

    typedJwtExpiresIn

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -8,6 +8,7 @@ import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtAuthGuard } from './jwt-auth.guard';
+import { type StringValue } from 'ms';
 
 @Module({
   imports: [
@@ -21,7 +22,7 @@ import { JwtAuthGuard } from './jwt-auth.guard';
       useFactory: () => {
         return {
           secret: process.env.JWT_SECRET,
-          signOptions: { expiresIn: '60s' },
+          signOptions: { expiresIn: process.env.EXPIRESIN as StringValue },
         };
       },
     }),
