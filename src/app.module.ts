41f0eaa8db957c27f169feebff255bdacc518f8d
HardFlame <gmhardflame@gmail.com>
2025-12-12 20:35:39 +0400

    winston logs boot now

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,38 +8,14 @@ import { TasksModule } from './tasks/task.module';
 import { DatabaseService } from './database/database.service';
 import { CronModule } from './cron/cron.module';
 import { ScheduleModule } from '@nestjs/schedule';
-import {
-  utilities as nestWinstonModuleUtilities,
-  WinstonModule,
-} from 'nest-winston';
-import winston from 'winston';
-import path from 'node:path';
+import { WinstonModule } from 'nest-winston';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.env`,
     }),
-    WinstonModule.forRoot({
-      format: winston.format.combine(
-        winston.format.timestamp(),
-        winston.format.ms(),
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        nestWinstonModuleUtilities.format.nestLike('Nest', {
-          colors: true,
-          prettyPrint: true,
-          processId: true,
-          appName: true,
-        }),
-      ),
-      transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({
-          dirname: path.join(__dirname, './../log/'),
-          filename: 'logs.log',
-        }),
-      ],
-    }),
+    WinstonModule.forRoot({}),
     ScheduleModule.forRoot(),
     DatabaseModule,
     CronModule,
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import {
+  // WINSTON_MODULE_NEST_PROVIDER,
+  WinstonModule,
+  utilities as nestWinstonModuleUtilities,
+} from 'nest-winston';
+import winston from 'winston';
+import path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.ms(),
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        nestWinstonModuleUtilities.format.nestLike('Nest', {
+          colors: true,
+          prettyPrint: true,
+          processId: true,
+          appName: true,
+        }),
+      ),
+      transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({
+          dirname: path.join(__dirname, './../log/'),
+          filename: 'logs.log',
+        }),
+      ],
+    }),
+  });
   await app.listen(process.env.PORT ?? 3000);
-  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.enableShutdownHooks();
 }
 bootstrap();
